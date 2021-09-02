284e8033ebcb1f12037ca51baf37ab734be50ba2
loracy <lora860227@gmail.com>
2021-09-02 20:25:19 +0800

    1. Convert the icons to font library in order to make them flexible for changing colo 2. Adjustments-1 選單

---
+++ js/general.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.DropdownInner-item > .dropdown-item.icon-arrow').each(function(index) {
+        
+        $(this).hover(function() {
+            $('.DropdownInner[category="' + (index + 1) + '"]').addClass('Show');
+        }, function() {
+            let isHoverOnChild = $('.DropdownInner[category="' + (index + 1) + '"]:hover').length > 0;
+            
+            if(!isHoverOnChild) {
+                $('.DropdownInner[category="' + (index + 1) + '"]').removeClass('Show');
+            }
+        });
+    });
+});
